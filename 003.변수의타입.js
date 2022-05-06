a667de9ec400ca9ff5a0c2e21d5dad0072376ded
taemin <zhfhldp1@naver.com>
2022-05-06 23:28:52 +0900

    first commit

---
+++ 003.변수의타입.js
@@ -0,0 +1,4 @@
+//다음 출력 값으로 올바른 것은?
+var arr = [100, 200, 300];
+console.log(typeof arr);
+// object = 배열(Array)는 오브젝트다
---
+++ 004.변수의 타입.js
@@ -0,0 +1,8 @@
+/* 
+다음 변수 a를 typeof(a)로 넣었을 때 출력될 값과의 연결이 알맞지 않은 것은?
+1)  입력 : a =1,   출력 : number
+2)  입력 : a = 2.22,   출력 : boolean
+3)  입력 : a = 'p',   출력 : string
+4)  입력 : a = [1, 2, 3],   출력 : object
+= 2번 boolean = 0,-0 ,null, false, NaN,undefined, ""(빈문자열), 문자열 false는 제외다.
+*/
